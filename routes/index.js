e0f8993cf5fabf92cbe9279d85f7aeaba81a1c2e
Graham <g_vickers@fanshaweonline.ca>
2019-12-15 23:03:05 -0500

    made a new demo for heroku

--- routes/index.js
+++ routes/index.js
@@ -3,57 +3,65 @@ var router = express.Router();
 const connect = require('../utils/sqlConnect');
 
 
-router.get('/', (req, res) => {
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log('at the main route');
 
-  // get the connection via the connection pool, and then run the query -> just one added step
-  connect.getConnection((err, connection) => {
-    if (err) { return console.log(error.message); }
+  res.render('layout.hbs', { title: 'Express' });
 
-    let query = `SELECT id, logo FROM tbl_projects`;
-
-    connect.query(query, (err, portfoliodata) => {
-    connection.release(); // send this connection back to the pool
-
-    if (err) {
-      // will exit the function and log the error
-      return console.log(err.message);
-
-    }
-
-    console.log('data: ', portfoliodata); // this should be your database query result
-
-    // render our page
-    res.render('layout.hbs', {data: portfoliodata}); // whatever page and data you're rendering
-   });
-  })
 })
 
+// router.get('/', (req, res) => {
+
+//   // get the connection via the connection pool, and then run the query -> just one added step
+//   connect.getConnection((err, connection) => {
+//     if (err) { return console.log(error.message); }
+
+//     let query = `SELECT id, logo FROM tbl_projects`;
+
+//     connect.query(query, (err, portfoliodata) => {
+//     connection.release(); // send this connection back to the pool
+
+//     if (err) {
+//       // will exit the function and log the error
+//       return console.log(err.message);
+
+//     }
+
+//     console.log('data: ', portfoliodata); // this should be your database query result
+
+//     // render our page
+//     res.render('layout.hbs', {data: portfoliodata}); // whatever page and data you're rendering
+//    });
+//   })
+// })
 
 
-router.get('/project/:target', (req, res) => {
 
-  // get the connection via the connection pool, and then run the query -> just one added step
-  connect.getConnection((err, connection) => {
-  if (err) { return console.log(error.message); }
+// router.get('/project/:target', (req, res) => {
 
-    let query = `SELECT id, title, img, bio FROM tbl_projects WHERE id="${req.params.target}"`;
+//   // get the connection via the connection pool, and then run the query -> just one added step
+//   connect.getConnection((err, connection) => {
+//   if (err) { return console.log(error.message); }
 
-    connect.query(query, (err, projectdata) => {
-      connection.release(); // send this connection back to the pool
+//     let query = `SELECT id, title, img, bio FROM tbl_projects WHERE id="${req.params.target}"`;
 
-      if (err) {
-        // will exit the function and log the error
-        return console.log(err.message);
-      }
+//     connect.query(query, (err, projectdata) => {
+//       connection.release(); // send this connection back to the pool
 
-      console.log('data: ', projectdata); // this should be your database query result
+//       if (err) {
+//         // will exit the function and log the error
+//         return console.log(err.message);
+//       }
 
-      // render our page
-      res.render('layout.hbs/project/:target', {data: projectdata}); // whatever page and data you're rendering
+//       console.log('data: ', projectdata); // this should be your database query result
 
-    });
-  })
-})
+//       // render our page
+//       res.render('layout.hbs/project/:target', {data: projectdata}); // whatever page and data you're rendering
+
+//     });
+//   })
+// })
 
 
 module.exports = router;
