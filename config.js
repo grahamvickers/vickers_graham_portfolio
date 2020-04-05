56806f2217a2ae9dcb355adf99b3bb9346fa7e41
Graham <g_vickers@fanshaweonline.ca>
2020-04-05 13:05:02 -0400

    converting for heroku

--- config.js
+++ config.js
@@ -11,9 +11,9 @@
 const config = {
     host: "us-cdbr-iron-east-01.cleardb.net",
     port: 8889,
-    uname: "be21db2776909a",
-    pword: "d523a3b4",
-    database: "heroku_7ae8274270701a0"
+    uname: "b112465bae77ca",
+    pword: "0f1f89de",
+    database: "heroku_770c2dc6568104b"
 }
 
 module.exports = config;
