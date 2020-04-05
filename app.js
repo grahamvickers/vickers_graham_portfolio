298021ed4c53a722e5796be059c097d369266605
Graham <g_vickers@fanshaweonline.ca>
2020-04-05 14:08:52 -0400

    converting for heroku

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var nodemailer = require('nodemailer');
-var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
